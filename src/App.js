b673e3ffcfe5da8eb8502980cfc44d1b1844bf5a
Mohammed Aabed <mohmmed.aabed@gmail.com>
2021-03-01 12:36:26 +0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import SingleColor from './SingleColor';
+
+import Values from 'values.js';
+
+function App() {
+  const [color, setColor] = useState('');
+  const [error, setError] = useState(false);
+  const [list, setList] = useState(new Values('#118add').all(10));
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      const colors = new Values(color).all(10);
+      setList(colors);
+    } catch (err) {
+      setError(true);
+    }
+  };
+
+  return (
+    <>
+      <section className='container'>
+        <h3>color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type='text'
+            value={color}
+            onChange={(e) => {
+              setColor(e.target.value);
+            }}
+            placeholder='#118add'
+            className={error ? 'error' : null}
+          />
+          <button className='btn' type='submit'>
+            submit
+          </button>
+        </form>
+      </section>
+
+      <section className='colors'>
+        {list.map((color, index) => {
+          return (
+            <SingleColor
+              key={index}
+              {...color}
+              index={index}
+              hexColor={color.hex}
+            />
+          );
+        })}
+      </section>
+    </>
+  );
+}
+
+export default App;
